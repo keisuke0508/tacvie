2df17394680007ab226fc9f92abd4665e091a46e
keisuke0508 <s14404ks@sfc.keio.ac.jp>
2017-12-06 00:27:59 +0900

    to change branch

---
+++ test.py
@@ -0,0 +1,12 @@
+import Tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    canvas = tk.Canvas(width=500, height=500)
+    canvas.pack()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
