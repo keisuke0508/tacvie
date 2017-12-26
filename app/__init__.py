894b65e06822e958bc6f3a3c910f35cfacf5c097
keisuke0508 <s14404ks@sfc.keio.ac.jp>
2017-12-27 02:02:16 +0900

    make log func

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from datetime import datetime
 import constant
+from connector import TacvieMain
 
 
 class TacvieAppBase:
@@ -15,6 +17,7 @@ class TacvieAppBase:
         self.set_title()
         self.set_movie_list()
         self.set_button()
+        self.set_log_area()
 
     def set_title(self):
         title = constant.APP_TITLE
@@ -36,14 +39,54 @@ class TacvieAppBase:
         end_btn = tk.Button(text=constant.END_BTN_TEXT)
         start_btn.bind(constant.BTN_EVENT, self.start_btn_func)
         end_btn.bind(constant.BTN_EVENT, self.end_btn_func)
-        start_btn.place(x=constant.BTN_X, y=constant.START_BTN_Y)
-        end_btn.place(x=constant.BTN_X, y=constant.END_BTN_Y)
+        start_btn.place(x=constant.START_BTN_X, y=constant.START_BTN_Y)
+        end_btn.place(x=constant.END_BTN_X, y=constant.END_BTN_Y)
+
+    def set_log_area(self):
+        self.logs = []
+        font = constant.FONT
+        size = constant.LOG_FONT_SIZE
+        log_area = tk.Label(text=None, font=(font, size))
+        log_area.place(x=constant.LOG_AREA_X, y=constant.LOG_AREA_Y)
 
     def start_btn_func(self, e):
-        print self.movie.get()
+        movie = self.movie.get()
+        if movie == constant.NOT_SELECT:
+            err_log = constant.LOG_NOT_SELECTED
+            self.write_log(err_log)
+        elif movie == constant.HAPTIC_MOVIE:
+            TacvieMain().haptic_senbay_ver()
+        elif movie == constant.BICYCLE_MOVIE:
+            TacvieMain().bicycle_senbay_speed_ver()
+        else:
+            err_log = constant.LOG_INVALID
+            self.write_log(err_log)
 
     def end_btn_func(self, e):
         pass
+
+    def write_log(self, log):
+        log = self.get_log(log)
+        self.update_log(log)
+        font = constant.FONT
+        size = constant.LOG_FONT_SIZE
+        logs = str()
+        for l in self.logs:
+            logs = logs + '\n' + l
+        log_area = tk.Label(text=logs, font=(font, size))
+        log_area.place(x=constant.LOG_AREA_X, y=constant.LOG_AREA_Y)
+
+    def get_log(self, log):
+        current_time = self.get_current_time()
+        return current_time + ':\n' + log
+
+    def update_log(self, log):
+        self.logs.append(log)
+        if(len(self.logs) > constant.LOG_LIMIT):
+            self.logs.pop(0)
+
+    def get_current_time(self):
+        return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
 
 class TacvieApp(TacvieAppBase):
--- app/constant.py
+++ app/constant.py
@@ -6,7 +6,7 @@ TITLE_FONT_SIZE = 50
 FONT = 'Helvetica'
 
 # window
-WINDOW_SIZE = '1200x700'
+WINDOW_SIZE = '300x500'
 
 # movie_list
 STATE = 'readonly'
@@ -15,13 +15,22 @@ OPENING_MOVIE_SETTING = 0
 NOT_SELECT = 'Not Select'
 HAPTIC_MOVIE = 'haptic.mov'
 BICYCLE_MOVIE = 'bicycle.mov'
-MOVIE_LIST_X = 30
-MOVIE_LIST_Y = 200
+MOVIE_LIST_X = 50
+MOVIE_LIST_Y = 150
 
 # button
 START_BTN_TEXT = 'START'
 END_BTN_TEXT = 'END'
 BTN_EVENT = '<Button-1>'
-BTN_X = 30
-START_BTN_Y = 250
-END_BTN_Y = 300
+START_BTN_X = 50
+START_BTN_Y = 200
+END_BTN_X = 150
+END_BTN_Y = 200
+
+# log_area
+LOG_FONT_SIZE = 15
+LOG_AREA_X = 50
+LOG_AREA_Y = 250
+LOG_NOT_SELECTED = 'movie is not selected.'
+LOG_INVALID = 'invalid'
+LOG_LIMIT = 5
